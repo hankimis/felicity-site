e02950183f71c9e2166a4e245ab3603ace6e39b1
hankimis <hankim.masion@gmail.com>
2025-03-23 23:07:54 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Vercel에서 ESLint 에러 무시
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
