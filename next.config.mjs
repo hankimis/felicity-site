de1c9cf57b2e991c64afa8dddef84681eda23e86
김재환 <hankim@gimjaehwan-ui-MacBookPro.local>
2025-03-30 20:42:31 +0900

    https로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,22 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googletagmanager.com https://*.google-analytics.com https://*.firebaseio.com https://*.firebase.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+                https://*.googleapis.com 
+                https://*.gstatic.com 
+                https://*.google.com 
+                https://*.googletagmanager.com 
+                https://*.google-analytics.com 
+                https://*.firebaseio.com 
+                https://*.firebase.com
+                https://www.googletagmanager.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' blob: data: https://*.googleapis.com https://*.gstatic.com https://*.google.com https://www.google.com;
+              img-src 'self' blob: data: 
+                https://*.googleapis.com 
+                https://*.gstatic.com 
+                https://*.google.com 
+                https://www.google.com
+                https://www.googletagmanager.com;
               font-src 'self' data: https://fonts.gstatic.com;
               connect-src 'self' 
                 https://*.firebaseio.com 
