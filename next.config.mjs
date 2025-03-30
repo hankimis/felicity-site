98134dd2272d0a227142a81cdc08f9d41592bf16
김재환 <hankim@gimjaehwan-ui-MacBookPro.local>
2025-03-30 20:33:24 +0900

    https로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -13,60 +13,20 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: `
-              default-src 'self' *.vercel.app *.vercel.com;
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' 
-                *.googleapis.com 
-                *.gstatic.com 
-                *.google.com
-                *.google-analytics.com
-                *.googletagmanager.com
-                *.firebaseio.com 
-                *.firebase.com
-                *.vercel.app
-                *.vercel.com;
-              style-src 'self' 'unsafe-inline' 
-                *.googleapis.com
-                *.vercel.app
-                *.vercel.com;
-              img-src 'self' blob: data: 
-                *.googleapis.com 
-                *.gstatic.com 
-                *.google.com
-                *.google-analytics.com
-                *.vercel.app
-                *.vercel.com;
-              font-src 'self' data: 
-                *.gstatic.com
-                *.vercel.app
-                *.vercel.com;
-              connect-src 'self' 
-                *.firebaseio.com 
-                *.googleapis.com 
-                *.google.com 
-                *.google-analytics.com
-                *.analytics.google.com
-                *.gstatic.com 
-                *.cloudfunctions.net
-                *.firebase.com
-                *.firebaseapp.com
-                *.firebase.database.app
-                *.vercel.app
-                *.vercel.com
-                wss://*.firebaseio.com
-                wss://*.firestore.googleapis.com;
-              frame-src 'self' 
-                *.firebaseapp.com 
-                *.google.com
-                *.vercel.app
-                *.vercel.com;
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https:;
+              style-src 'self' 'unsafe-inline' https:;
+              img-src 'self' blob: data: https:;
+              font-src 'self' data: https:;
+              connect-src 'self' https:;
+              frame-src 'self' https:;
               worker-src 'self' blob:;
-              media-src 'self' 
-                *.vercel.app
-                *.vercel.com;
+              media-src 'self' https:;
               manifest-src 'self';
               object-src 'none';
               base-uri 'self';
               form-action 'self';
+              upgrade-insecure-requests;
             `.replace(/\s+/g, ' ').trim()
           },
           {
