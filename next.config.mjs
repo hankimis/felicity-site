6165d4a8f12ce66c0c7ead9e6fe06021f7de47dd
김재환 <hankim@gimjaehwan-ui-MacBookPro.local>
2025-03-30 20:09:33 +0900

    오류수정 22

--- next.config.mjs
+++ next.config.mjs
@@ -14,13 +14,35 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.google.com https://*.googleapis.com https://*.gstatic.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.firebaseio.com https://*.firebase.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' blob: data: https://*.google.com https://*.googleapis.com https://*.gstatic.com;
+              img-src 'self' blob: data: https://*.googleapis.com https://*.gstatic.com https://*.google.com;
               font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://*.googleapis.com https://*.google.com https://*.gstatic.com https://*.firebaseio.com https://*.cloudfunctions.net;
-              frame-src 'self' https://*.google.com;
+              connect-src 'self' 
+                https://*.firebaseio.com 
+                https://*.googleapis.com 
+                https://*.google.com 
+                https://*.gstatic.com 
+                https://*.cloudfunctions.net
+                https://*.firebase.com
+                https://*.firebase.database.app
+                wss://*.firebaseio.com
+                wss://*.firebase.database.app;
+              frame-src 'self' https://*.firebaseapp.com https://*.google.com;
+              worker-src 'self' 'blob:';
             `.replace(/\s+/g, ' ').trim()
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization'
           }
         ]
       }
