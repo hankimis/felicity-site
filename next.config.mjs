ecfc2cedf02316e6a1a1fabc7d8b5630f1784722
김재환 <hankim@gimjaehwan-ui-MacBookPro.local>
2025-03-30 20:16:58 +0900

    vercel 수정

--- next.config.mjs
+++ next.config.mjs
@@ -13,11 +13,29 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.firebaseio.com https://*.firebase.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' blob: data: https://*.googleapis.com https://*.gstatic.com https://*.google.com;
-              font-src 'self' https://fonts.gstatic.com;
+              default-src 'self' vercel.app vercel.com *.vercel.app *.vercel.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' 
+                https://*.googleapis.com 
+                https://*.gstatic.com 
+                https://*.google.com 
+                https://*.firebaseio.com 
+                https://*.firebase.com
+                https://*.vercel.app
+                https://*.vercel.com;
+              style-src 'self' 'unsafe-inline' 
+                https://fonts.googleapis.com 
+                https://*.vercel.app
+                https://*.vercel.com;
+              img-src 'self' blob: data: 
+                https://*.googleapis.com 
+                https://*.gstatic.com 
+                https://*.google.com
+                https://*.vercel.app
+                https://*.vercel.com;
+              font-src 'self' data: 
+                https://fonts.gstatic.com
+                https://*.vercel.app
+                https://*.vercel.com;
               connect-src 'self' 
                 https://*.firebaseio.com 
                 https://*.googleapis.com 
@@ -25,11 +43,24 @@ const nextConfig = {
                 https://*.gstatic.com 
                 https://*.cloudfunctions.net
                 https://*.firebase.com
+                https://*.firebaseapp.com
                 https://*.firebase.database.app
                 wss://*.firebaseio.com
-                wss://*.firebase.database.app;
-              frame-src 'self' https://*.firebaseapp.com https://*.google.com;
-              worker-src 'self' 'blob:';
+                wss://*.firebase.database.app
+                https://*.vercel.app
+                https://*.vercel.com
+                wss://*.vercel.app
+                wss://*.vercel.com;
+              frame-src 'self' 
+                https://*.firebaseapp.com 
+                https://*.google.com
+                https://*.vercel.app
+                https://*.vercel.com;
+              worker-src 'self' blob:;
+              media-src 'self' 
+                https://*.vercel.app
+                https://*.vercel.com;
+              manifest-src 'self';
             `.replace(/\s+/g, ' ').trim()
           },
           {
